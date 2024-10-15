e812b6aa2d5293b0b9ee2a4702c5d160a3947055
CHEN Boying <boyingc963@gmail.com>
2024-10-15 19:15:40 +0800

    make some change in plot.py

--- project2/src/plot.py
+++ project2/src/plot.py
@@ -1,75 +1,8 @@
-# import matplotlib.pyplot as plt
-# import re
-
-# # Read data from the file
-# with open('./result-multi.txt', 'r') as file:
-#     data = file.read()
-
-# # Split the data into datasets based on the "Dataset loaded" delimiter
-# datasets = data.strip().split('Dataset loaded:')
-# datasets = [ds for ds in datasets if ds.strip()]  # Remove any empty strings
-
-# all_data = []
-
-# for ds in datasets:
-#     lines = ds.strip().split('\n')
-#     header = lines[0].strip()
-#     # Extract dimension for labeling
-#     dimension_match = re.search(r'dimension:\s*(\d+)', header)
-#     if dimension_match:
-#         dimension = dimension_match.group(1)
-#     else:
-#         dimension = 'Unknown'
-    
-#     lambdas = []
-#     margins = []
-#     current_lambda = None
-#     current_margin = None
-    
-#     # Iterate over the lines to extract lambda and margin values
-#     for line in lines[1:]:
-#         lambda_match = re.match(r'lambda\s*=\s*([\d.]+)', line)
-#         margin_match = re.match(r'Final margin after convergence:\s*([\d.]+)', line)
-#         if lambda_match:
-#             current_lambda = float(lambda_match.group(1))
-#             lambdas.append(current_lambda)
-#         elif margin_match:
-#             current_margin = float(margin_match.group(1))
-#             margins.append(current_margin)
-    
-#     # Ensure the number of lambdas and margins match
-#     if len(lambdas) != len(margins):
-#         print(f"Warning: Mismatched data in dataset with dimension {dimension}.")
-#         continue
-    
-#     dataset_label = f'Dimension {dimension}'
-#     all_data.append({
-#         'label': dataset_label,
-#         'lambdas': lambdas,
-#         'margins': margins
-#     })
-
-# # Plot the data
-# plt.figure(figsize=(10, 6))
-
-# for dataset in all_data:
-#     plt.plot(dataset['lambdas'], dataset['margins'], marker='o', label=dataset['label'])
-
-# plt.xlabel('Lambda')
-# plt.ylabel('Final Margin After Convergence')
-# plt.title('Lambda vs Final Margin for Multiple Datasets')
-# plt.legend()
-# plt.grid(True)
-
-# plt.gca().invert_xaxis()
-
-# plt.show()
-
 import matplotlib.pyplot as plt
 import re
 
 # Read data from the file
-with open('./result-multi.txt', 'r') as file:
+with open('./result.txt', 'r') as file:
     data = file.read()
 
 # Split the data into datasets based on the "Dataset loaded" delimiter
@@ -117,7 +50,7 @@ for ds in datasets:
     })
 
 # Plot the data
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(20, 12))
 
 for dataset in all_data:
     lambdas = dataset['lambdas']
@@ -153,4 +86,6 @@ plt.grid(True)
 # Invert the x-axis to show lambda from large to small
 plt.gca().invert_xaxis()
 
+# Save the figure as 'fig.png'
+plt.savefig('fig.png', dpi=500)
 plt.show()
